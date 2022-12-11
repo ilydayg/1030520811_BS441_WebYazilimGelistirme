ba177e78637078cbe398630757623c5779819660
İlayda Yegen <83966876+ilydayg@users.noreply.github.com>
2022-12-11 16:17:31 +0300

    değişiklikler yapıldı

---
+++ odev/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>TAŞ KAĞIT MAKAS</h1>
+
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
